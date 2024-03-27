4f83155ab262a708421c88bc9bdb5eec52c85a05
AnirudhKaranth <anikaranth47@gmail.com>
2024-03-27 23:36:33 +0530

    initial setup

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,16 @@
+
+
+<nav>
+    <ul>
+        <li>
+            <a href="/">Home</a>
+
+        </li>
+        <li>
+
+            <a href="/posts">posts</a>
+        </li>
+    </ul>
+</nav>
+
+<slot/>
